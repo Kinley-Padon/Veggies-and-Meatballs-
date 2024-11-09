c43b36fca63462456ffed1c59e5cc291a51e6203
Kinley-Padon <145949013+Kinley-Padon@users.noreply.github.com>
2024-11-09 14:53:31 -0500

    "minimal recipe entity."

--- src/main/java/entities/Recipes.java
+++ src/main/java/entities/Recipes.java
@@ -1,1 +1,25 @@
+package entity
 
+/**
+ * Represents a recipe with its name and ID.
+ */
+
+public class Recipes{
+
+    private int ID;
+    private String Name;
+
+    public Recipes(int ID, String Name){
+        this.ID = ID;
+        this.Name = Name;
+    }
+
+    public int getname(){
+        return name;
+    }
+
+    public int getID(){
+        return ID;
+    }
+
+}
