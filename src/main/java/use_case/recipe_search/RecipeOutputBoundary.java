04d08baa34d3b9b379d761c2bee7745a474a71a2
De_zangmo <de.zangmo@mail.utoronto.ca>
2024-11-16 13:43:25 -0500

    Updated RecipeOutputBoundary.java

--- src/main/java/use_case/recipe_search/RecipeOutputBoundary.java
+++ src/main/java/use_case/recipe_search/RecipeOutputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.recipe_search;
+
+import entities.Recipes;
+
+import java.util.List;
+
 /**
  * The output boundary for the Recipe search Use Case.
  */
@@ -7,9 +12,9 @@ public interface RecipeOutputBoundary {
 
     /**
      * Prepares the success view for the Search related Use Cases.
-     * @param message the output data
+     * @param recipeContents the output data
      */
-    void prepareSuccessView(String message);
+    void prepareSuccessView(List<Recipes> recipeContents);
 
     /**
      * Prepares the failure view for the Search related Use Cases.
