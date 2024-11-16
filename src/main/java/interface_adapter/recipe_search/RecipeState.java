31eafa91917b9d17be81c22a98eab50aa43018ee
jerry <jerrrrrrry.wang@mail.utoronto.ca>
2024-11-16 15:47:32 -0500

    Update interface_adapter

--- src/main/java/interface_adapter/recipe_search/RecipeState.java
+++ src/main/java/interface_adapter/recipe_search/RecipeState.java
@@ -2,18 +2,33 @@ package interface_adapter.recipe_search;
 
 import entities.Recipes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RecipeState {
     private List<Recipes> recipeContents;
     private String errorMessage;
 
-    public String getRecipeDetails() {
-        return recipeContents;
+    public HashMap<String, Integer> getRecipeDetails() {
+        HashMap<String, Integer> recipeDetails = null;
+        if (recipeContents != null) {
+            recipeDetails = new HashMap<>();
+            for (Recipes recipe : recipeContents) {
+                recipeDetails.put(recipe.getName(), recipe.getID()); // Add recipe name and ID to the map
+            }
+        }
+        return recipeDetails;
     }
 
-    public void setRecipeDetails(String recipeDetails) {
-        this.recipeContents; = recipeDetails;
+    public void setRecipeDetails(List<Recipes> recipeDetails) {
+        HashMap<String, Integer> recipeDetail = null;
+        if (recipeContents != null) {
+            recipeDetail = new HashMap<>();
+            for (Recipes recipe : recipeDetails) {
+                recipeDetail.put(recipe.getName(), recipe.getID()); // Add recipe name and ID to the map
+            }
+        }
     }
 
     public String getErrorMessage() {
