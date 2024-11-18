83c1a09a800568d79f38a1f7f9d6d57584b11c3d
De_zangmo <de.zangmo@mail.utoronto.ca>
2024-11-18 10:08:54 -0500

    RecipeAddDataAccessException.java and RecipeAddDataAccessInterface.java

---
+++ src/main/java/use_case/recipe_add/RecipeAddDataAccessException.java
@@ -0,0 +1,7 @@
+package use_case.recipe_add;
+
+public class RecipeAddDataAccessException extends RuntimeException {
+    public RecipeAddDataAccessException(String message) {
+        super(message);
+    }
+}
