f60a259501fdd4aad3e4219318dc860b412f79ff
DeZangmo <de.zangmo@mail.utoronto.ca>
2024-11-22 17:46:03 -0500

    Updating changes in main (#32)
    * Step 1:
    
    "add receipe detailed view and images"
    
    * Step 1:
    
    "add receipe detailed view and images"
    
    * Step2:
    "add substrituoin api & show ingredients substiations"
    
    * 'added user'
    
    ---------
    
    Co-authored-by: jerry <jerrrrrrry.wang@mail.utoronto.ca>
    Co-authored-by: Kinley-Padon <145949013+Kinley-Padon@users.noreply.github.com>

---
+++ src/main/java/interface_adapter/logout/LogoutPresenter.java
@@ -0,0 +1,54 @@
+
+package interface_adapter.logout;
+
+import interface_adapter.ViewManagerModel;
+import interface_adapter.change_password.LoggedInState;
+import interface_adapter.change_password.LoggedInViewModel;
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
+import use_case.logout.LogoutOutputBoundary;
+import use_case.logout.LogoutOutputData;
+
+/**
+ * The Presenter for the Logout Use Case.
+ */
+public class LogoutPresenter implements LogoutOutputBoundary {
+
+    private final LoggedInViewModel loggedInViewModel;
+    private final ViewManagerModel viewManagerModel;
+    private final LoginViewModel loginViewModel;
+
+    public LogoutPresenter(ViewManagerModel viewManagerModel,
+                           LoggedInViewModel loggedInViewModel,
+                           LoginViewModel loginViewModel) {
+        // Assigning instance variables
+        this.viewManagerModel = viewManagerModel;
+        this.loggedInViewModel = loggedInViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LogoutOutputData response) {
+        // Update the LoggedInState
+        LoggedInState loggedInState = loggedInViewModel.getState();
+        loggedInState.setUsername("");
+        loggedInViewModel.setState(loggedInState);
+        loggedInViewModel.firePropertyChanged();
+
+        // Update the LoginState
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsername("");
+        loginState.setPassword("");
+        loginViewModel.setState(loginState);
+        loginViewModel.firePropertyChanged();
+
+        // Switch to the LoginView
+        this.viewManagerModel.setState(loginViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        // No need to add code here. We'll assume that logout can't fail.
+    }
+}
